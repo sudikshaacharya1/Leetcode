e63696692e2043b56d37a7574a4124b7268769e0
Sudiksha Acharya <138321124+sudikshaacharya1@users.noreply.github.com>
2024-02-03 18:19:58 -0500

    Time: 403 ms (98.01%) | Memory: 32 MB (61.02%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -2,10 +2,19 @@ class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         #If we compare each elements Time: o(nˆ2) Space: o(1)
         #If we do sort then compare Time: o(nlogn) Space: o(1)
-        nums1 = sorted(nums)
-        for i in range(len(nums1) - 1):
-            if nums1[i] == nums1[i + 1]:
+        #nums1 = sorted(nums)
+        #for i in range(len(nums1) - 1):
+        #    if nums1[i] == nums1[i + 1]:
+        #        return True
+        #return False
+
+        ##IF we use hashset, Time: o(n) Space: o(n)
+        hashset = set()
+
+        for i in nums:
+            if i in hashset:
                 return True
+            hashset.add(i)
         return False
         
         
